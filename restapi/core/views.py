f3bfec566e63fa9f096fc9f9db96b60a2f18ba39
gustavocioccari <gustavocioccari@gmail.com>
2020-06-11 17:14:43 -0300

    Create user and salary viewsets

--- restapi/core/views.py
+++ restapi/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer, SalarySerializer
+from .models import User, Salary
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SalaryViewSet(viewsets.ModelViewSet):
+    queryset = Salary.objects.all()
+    serializer_class = SalarySerializer
