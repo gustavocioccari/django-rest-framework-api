25ffd203ec7ae1e4ac24398245b7e9dee8c5d20f
gustavocioccari <gustavocioccari@gmail.com>
2020-06-11 17:04:01 -0300

    Create user and salary models

--- restapi/core/models.py
+++ restapi/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    cpf = models.CharField(max_length=11)
+    birth = models.DateField(max_length=8)
+
+    def __str__(self):
+        return self.name
+
+class Salary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    salary = models.FloatField()
+    discounts = models.FloatField()
+    payday = models.FloatField()
