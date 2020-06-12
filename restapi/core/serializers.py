cb01f9e503224c659f5753c2d6bd2ae306848eb8
gustavocioccari <gustavocioccari@gmail.com>
2020-06-12 10:25:42 -0300

    Create salary data serializer and view

--- restapi/core/serializers.py
+++ restapi/core/serializers.py
@@ -1,5 +1,6 @@
 from .models import User, Salary
 from rest_framework import serializers
+from django.db.models import Avg, Max, Min
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,28 @@ class SalarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Salary
         fields = '__all__'
+
+class SalaryDataSerializer(serializers.ModelSerializer):
+    avg_salary = serializers.SerializerMethodField()
+    avg_discounts = serializers.SerializerMethodField()
+    max_salary = serializers.SerializerMethodField()
+    min_salary = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Salary
+        fields = ['avg_salary', 'max_salary', 'min_salary', 'avg_discounts']
+
+    def get_avg_salary(self,obj):
+        avgsalary = Salary.objects.filter(user_id=obj.user).values_list('pk',flat=True).aggregate(avg_salary=Avg('salary'))
+        return avgsalary['avg_salary']
+    def get_avg_discounts(self,obj):
+        avgdiscounts = Salary.objects.filter(user_id=obj.user).aggregate(avg_discounts=Avg('discounts'))
+        return avgdiscounts['avg_discounts']
+
+    def get_max_salary(self,obj):
+        maxsalary = Salary.objects.filter(user_id=obj.user).aggregate(max_salary=Max('salary'))
+        return maxsalary['max_salary']
+
+    def get_min_salary(self,obj):
+        minsalary = Salary.objects.filter(user_id=obj.user).aggregate(min_salary=Min('salary'))
+        return minsalary['min_salary']
--- restapi/core/views.py
+++ restapi/core/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Avg, Count, Max
 from rest_framework import viewsets
-from .serializers import UserSerializer, SalarySerializer
+from .serializers import UserSerializer, SalarySerializer, SalaryDataSerializer
 from .models import User, Salary
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class SalaryViewSet(viewsets.ModelViewSet):
     queryset = Salary.objects.all()
     serializer_class = SalarySerializer
+
+class SalaryDataViewSet(viewsets.ModelViewSet):
+    serializer_class = SalaryDataSerializer
+    def get_queryset(self, *args, **kwargs):
+        return Salary.objects.filter(user_id=self.request.GET.get('user_pk'))
+    
